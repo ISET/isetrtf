286387fee0dba8e4865b5d7077ef5d3a1e192890
Thomas Goossens <thomas42@gray.stanford.edu>
2022-02-11 07:20:42 -0800

    documentation

--- tracing/doesRayPassEllipse.m
+++ tracing/doesRayPassEllipse.m
@@ -1,9 +1,24 @@
-function pass = doesRayPassEllipse(rotatedOrigins,rotatedDirections,positions,radii,centers,intersectionplaneZ)
+function pass = doesRayPassEllipse(rotatedOrigins,rotatedDirections,positions,radii,centers,distanceToRayPassPlane)
 % Check that a ray makes it through the lens
 %
 % INPUTS
-%  ray_origins_oninputplane -  Nx3 vector (x,y,z) stating the origin of the
-%    ray on the input plane. 
+%  rotatedOrigins -  Nx3 vector (x,y,z) stating the origin of the
+%                    ray on the input plane after applying the rotation
+%                    matrix. N is the number of rays to process
+%                    simultaneously.
+%
+%  rotatedDirections - Nx3 direction vectors after applying the rotation
+%                      matrix.
+%
+%  positions - Nx1 vector containing all the input plane positions (distance from the center) at which the
+%              ellipses are defined. 
+%  radii - Nx2 matrix containing the radii (x,y) of the ellipses defined at
+%           each position
+%  centers - Nx2 matrix containing the centers (x,y) of the ellipses defined at
+%           each position
+%  distanceToRayPassPlane - Distance of ray pass plane from input plane.
+%                          The ray pass plane is the plane where the ellipses are defined
+%
 % Syntax:
 %   pass =doesRayPassEllipse(ray_origins_oninputplane,ray_directions,positions,radii,centers) 
 %
@@ -14,18 +29,15 @@ offaxisDistanceOnInputPlane=rotatedOrigins(:,2); % By construction only this val
 radius=interp1(positions,radii,offaxisDistanceOnInputPlane);
 center=interp1(positions,centers,offaxisDistanceOnInputPlane);
 
-
 % Project to circle plane
-alpha = (intersectionplaneZ)./(rotatedDirections(:,3));
-pointOnIntersectPlane = rotatedOrigins + alpha.*rotatedDirections; 
+alpha = (distanceToRayPassPlane)./(rotatedDirections(:,3));
+pointOnRayPassPlane = rotatedOrigins + alpha.*rotatedDirections; 
 
-% All points projected to the intersectionplane 
-
-pass = pointInEllipse(radius,center,pointOnIntersectPlane(:,1),pointOnIntersectPlane(:,2));
+% All points projected to the raypassplane 
+pass = pointInEllipse(radius,center,pointOnRayPassPlane(:,1),pointOnRayPassPlane(:,2));
 
 
 % Boolean AND operation, ray needs to pass through all
-
     function pass = pointInEllipse(radius,center,x,y)
                distX = (x-center(:,1));
                distY = (y-center(:,2));
